7b5672dbb8b9b6310d4051ba53a05b625bb3415a
gagan-gaurav <iamgagangaurav@gmail.com>
2021-11-07 20:09:33 +0530

    commit

--- personel_website/settings.py
+++ personel_website/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-# import django_heroku
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -159,4 +159,4 @@ os.makedirs(MEDIA_ROOT, exist_ok=True)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
