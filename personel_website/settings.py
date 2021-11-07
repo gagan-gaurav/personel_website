07cd219c7a1cf6024ef606ea8f293aa61030a020
gagan-gaurav <iamgagangaurav@gmail.com>
2021-11-07 18:52:57 +0530

    commit

--- personel_website/settings.py
+++ personel_website/settings.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'media')
 ]
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_URL = "/static/"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
